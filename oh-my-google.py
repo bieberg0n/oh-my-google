e7e029644ccc753439b4c75d5550cc141f2cb7f4
bjong <biebergong@gmail.com>
2017-02-28 21:24:36 +0800

    del comment

--- oh-my-google.py
+++ oh-my-google.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from gevent import monkey, spawn, queue, socket
+from gevent import monkey, spawn, queue  #, socket
 monkey.patch_socket()
 monkey.patch_ssl()
 import gevent.queue as queue
 import requests
-# import threading
 import re
 import json
-# import socks
-# from flask import Flask, request
-# from gevent.wsgi import WSGIServer
 from gevent.server import StreamServer
 
-# app = Flask(__name__)
 s = requests.session()
-# s.headers['User-Agent'] = 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 serv_q = queue.Queue()
 
 
@@ -27,7 +21,6 @@ host_p = re.compile('https*://.+?/')
 
 def get_headers_str(resp):
     headers_list = [key + ': ' + value for key, value in resp.headers.items()]
-    # headers_str = '\r\n'.join(headers_list)
     headers_head = 'HTTP/1.1 ' + str(resp.status_code) + ' OK'
     headers_str = '\r\n'.join([headers_head] + headers_list)
     return headers_str
@@ -37,9 +30,6 @@ def get_google():
     while True:
         url, headers, cli_q = serv_q.get()
         print(url)
-        # print(s.headers)
-        # s.headers = headers
-        # print(s.headers)
         try:
             print('requests start')
             status_code = 503
@@ -48,9 +38,6 @@ def get_google():
                 resp = s.get('https://www.google.com.hk{}'.format(url), headers=headers, timeout=3)
                 print(resp.status_code)
                 status_code = resp.status_code
-            # resp_headers_str = resp_headers_str.replace('gzip', '')\
-            #                                    .replace('chunked', '')\
-            #                                    .encode()
         # except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, socket.timeout):
         except:
             cli_q.put(b'')
@@ -69,14 +56,7 @@ def get_google():
 
 def get_headers_raw(conn):
     headers_raw = ''
-    # for buf in iter(lambda: conn.recv(1), b''):
-    # conn.settimeout(2)
-    # while not headers_raw.endswith('\r\n\r\n'):
-    # while not '\r\n\r\n' in headers_raw:
     for buf in iter(lambda: conn.recv(512), b''):
-        # buf = conn.recv(512)
-        # if not buf:
-        #     break
         print(buf)
         headers_raw += buf.decode('utf-8', errors='ignore')
         if '\r\n\r\n' in headers_raw:
@@ -86,8 +66,6 @@ def get_headers_raw(conn):
 
 
 def get_headers(conn):
-    # url = host_p.sub('', request.url)
-    # print(type(request.headers))
     headers_raw = get_headers_raw(conn)
     headers_dict = dict()
     headers_lines = headers_raw.split('\r\n')
@@ -99,17 +77,12 @@ def get_headers(conn):
             headers_dict[key] = value
         else:
             pass
-    # headers = {key: headers_raw[key] for key in request.headers.keys()}
-    # get_headers(request.headers)
-    # del headers['Host'], headers['Content-Length']
-    # print(headers)
     return headers_dict
 
 
 def handle(conn_cli, addr_cli):
     print(addr_cli)
     headers_dict = get_headers(conn_cli)
-    # print(headers_dict)
     if not headers_dict['method'] == 'GET':
         return
     else:
@@ -119,20 +92,13 @@ def handle(conn_cli, addr_cli):
         cli_q = queue.Queue()
         serv_q.put((path, headers_dict, cli_q))
         resp = cli_q.get()
-        # print(resp[:2000])
         resp = resp.replace(b'www.google.com.hk', host.encode())
         conn_cli.sendall(resp)
-    # print(conn_cli.recv(1024))
 
 
 def main(host, port, ssl_file=None):
-    # if debug:
-    #     app.run(host=host, port=port, debug=debug,
-    #             ssl_context=ssl_file, threaded=True)
-    # else:
     if ssl_file:
         certfile, keyfile = ssl_file
-        # WSGIServer((host, port), keyfile=keyfile, certfile=certfile).serve_forever()
         StreamServer((host, port), handle=handle, keyfile=keyfile, certfile=certfile).serve_forever()
     else:
         StreamServer((host, port), handle=handle).serve_forever()
